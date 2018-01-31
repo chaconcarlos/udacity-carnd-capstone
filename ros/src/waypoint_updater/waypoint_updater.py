9bf1ecda1549862fca8100cd98402627850f5081
Carlos Chacon <elcheicon@gmail.com>
2018-01-30 18:49:06 -0800

    Now the waypoint updater accelerates or decelerates according to the stop point. Still needs some work, the vehicle doesn't completely stops at the end, which can be an issue in the DBW node.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -26,10 +26,11 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 LOOP_RATE                   = 10 # Hz
 LOOKAHEAD_WPS               = 50 # 200
 DISTANCE_TO_CLOSEST         = 999999999
-STOP_DISTANCE               = 30
+STOP_DISTANCE               = 40
 METERS_PER_KILOMETER        = 1000
 SECONDS_PER_HOUR            = 3600
 MAX_ACCELERATION            = 1.0 # 8 m/s2 by a rate of 10 Hz
+MAX_DECELERATION            = 2   # m/s2
 NEXT_WAYPOINT_MAX_ANGLE     = math.pi / 4
 
 def to_meters_per_second(kilometers_per_hour):
@@ -43,6 +44,33 @@ def to_meters_per_second(kilometers_per_hour):
       float: The value in meters per second.
   """
   return (kilometers_per_hour * METERS_PER_KILOMETER) / SECONDS_PER_HOUR
+
+def get_distance(position_1, position_2):
+  """
+  Gets the distance squared between two positions.
+
+  Args:
+      position_1 (Point): The first point.
+      position_2 (Point): The second point.
+
+  Returns:
+      The distance between the given points.
+  """
+  x_coefficient = (position_1.x - position_2.x) ** 2
+  y_coefficient = (position_1.y - position_2.y) ** 2
+  z_coefficient = (position_1.z - position_2.z) ** 2
+
+  return math.sqrt(x_coefficient + y_coefficient  + z_coefficient)
+
+def set_waypoint_velocity(waypoint, velocity):
+  """
+  Sets the velocity of a waypoint.
+
+  Args:
+      waypoint (Lane): The waypoint to set the velocity to.
+      velocity (float): The velocity.
+  """
+  waypoint.twist.twist.linear.x = velocity
 
 class WaypointUpdater(object):
   """
@@ -71,12 +99,12 @@ class WaypointUpdater(object):
 
     rospy.logdebug("WaypointUpdater - Initializing variables...")
 
-    self.target_velocity        = to_meters_per_second(rospy.get_param('/waypoint_loader/velocity'))
-    self.current_pose           = None
-    self.current_velocity       = 0
-    self.map_waypoints          = None
-    self.last_closest_index     = 0
-    self.is_destination_reached = False
+    self.target_velocity     = to_meters_per_second(rospy.get_param('/waypoint_loader/velocity'))
+    self.current_pose        = None
+    self.current_velocity    = 0
+    self.map_waypoints       = None
+    self.last_closest_index  = 0
+    self.stop_waypoint_index = None
 
     rospy.logdebug("WaypointUpdater - Target_speed set to %s. m/s", self.target_velocity)
     rospy.loginfo("WaypointUpdater - Waypoint updater initialization finished.")
@@ -129,42 +157,6 @@ class WaypointUpdater(object):
     # TODO: Callback for /obstacle_waypoint message. We will implement it later
     pass
 
-  def get_waypoint_velocity(self, waypoint):
-    """
-    Handles the event when the an obstacle waypoint has been received.
-
-    Args:
-        msg (Lane): The message with the information of the obstacle position.
-    """
-    return waypoint.twist.twist.linear.x
-
-  def set_waypoint_velocity(self, waypoint, velocity):
-    """
-    Sets the velocity of a waypoint.
-
-    Args:
-        waypoint (Lane): The waypoint to set the velocity to.
-        velocity (float): The velocity.
-    """
-    waypoint.twist.twist.linear.x = velocity
-
-  def get_distance(self, position_1, position_2):
-    """
-    Gets the distance between two positions.
-
-    Args:
-        position_1 (Point): The first point.
-        position_2 (Point): The second point.
-
-    Returns:
-        The distance between the given points.
-    """
-    x_coefficient = (position_1.x - position_2.x) ** 2
-    y_coefficient = (position_1.y - position_2.y) ** 2
-    z_coefficient = (position_1.z - position_2.z) ** 2
-
-    return x_coefficient + y_coefficient  + z_coefficient
-
   def get_closest_waypoint_index(self):
     """
     Gets the index of the closest waypoint to the current position.
@@ -179,7 +171,7 @@ class WaypointUpdater(object):
     for i in range(self.map_waypoints_count - closest_index):
       index             = self.last_closest_index + i
       waypoint_position = self.map_waypoints[index].pose.pose.position 
-      distance          = self.get_distance(current_position, waypoint_position)
+      distance          = get_distance(current_position, waypoint_position)
 
       if (distance < current_min_distance):
         current_min_distance = distance
@@ -217,24 +209,75 @@ class WaypointUpdater(object):
     
     return closest_index
 
+  def get_next_stop_index(self, start_index):  
+    """
+    Gets the index of the next stop waypoint.
+
+    Args:
+        start_index (Int): The index of the starting waypoint.
+  
+    Returns:
+        The index of the next stop waypoint.
+    """
+    next_stop_index     = self.map_waypoints_count - 1
+    is_valid_stop_index = self.stop_waypoint_index != None and self.stop_waypoint_index > 0
+
+    if (is_valid_stop_index):
+      is_on_range     = self.stop_waypoint_index < self.map_waypoints_count
+      is_ahead        = self.stop_waypoint_index > start_index
+
+      if (is_on_range and is_ahead):
+      	next_stop_index = self.stop_waypoint_index
+  	
+    return next_stop_index
+
+  def accelerate(self, waypoints, target_velocity):
+    """
+    Sets the target velocity of the waypoints accelerating to the target velocity.
+
+    Args:
+        waypoints        (List): [out] The waypoints.
+        target_velocity (Float): The top speed to accelerate to.
+    """
+    for i in range(len(waypoints)):
+      waypoint_velocity = min(self.current_velocity + (i + 1) * MAX_ACCELERATION, target_velocity)
+      set_waypoint_velocity(waypoints[i], waypoint_velocity)
+
+  def decelerate(self, waypoints):    
+    """
+    Sets the target velocity of the waypoints decelerating to stop in the last waypoint.
+
+    Args:
+        waypoints        (List): [out] The waypoints.
+    """
+    stop_position = waypoints[len(waypoints) - 1].pose.pose.position
+
+    for waypoint in waypoints:
+      distance = get_distance(waypoint.pose.pose.position, stop_position)               
+      velocity = math.sqrt(2 * MAX_DECELERATION * distance)
+      set_waypoint_velocity(waypoint, velocity)
+
   def publish(self):
     """
     Publishes the updated waypoints of the route to follow.
     """
-    if (self.map_waypoints == None or self.current_pose == None):
-      return
-
     next_waypoint_index = self.get_next_waypoint_index()
-    look_ahead_index    = next_waypoint_index + LOOKAHEAD_WPS
+    next_stop_index     = self.get_next_stop_index(next_waypoint_index)
+    start_position      = self.map_waypoints[next_waypoint_index].pose.pose.position
+    stop_position       = self.map_waypoints[next_stop_index].pose.pose.position
+    stop_distance       = get_distance(start_position, stop_position)
+    future_waypoints    = []
 
-    if (look_ahead_index >= self.map_waypoints_count):
-      future_waypoints = self.map_waypoints[next_waypoint_index : ]
-    else:
+    if (next_stop_index != next_waypoint_index):
+      look_ahead_index = min(next_waypoint_index + LOOKAHEAD_WPS, next_stop_index)
       future_waypoints = self.map_waypoints[next_waypoint_index : look_ahead_index]
+    elif (stop_distance == 0):
+      future_waypoints.append(self.map_waypoints[next_waypoint_index])
 
-    for i in range(len(future_waypoints)):
-      waypoint_velocity = min(self.current_velocity + (i + 1) * MAX_ACCELERATION, self.target_velocity)
-      self.set_waypoint_velocity(future_waypoints[i], waypoint_velocity)
+    if (stop_distance <= STOP_DISTANCE):
+      self.decelerate(future_waypoints)
+    else:
+      self.accelerate(future_waypoints, self.target_velocity)
 
     lane                 = Lane()
     lane.header.frame_id = '/world'
@@ -252,7 +295,9 @@ class WaypointUpdater(object):
     rate = rospy.Rate(LOOP_RATE)
     
     while not rospy.is_shutdown():
-      self.publish()
+      if (self.map_waypoints != None and self.current_pose != None):
+        self.publish()
+
       rate.sleep()
 
 if __name__ == '__main__':
